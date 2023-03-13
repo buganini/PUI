ac6b92eb3140c2f78753510c2bec8d04d41a7286
Buganini Chiu <buganini@b612.tw>
2023-03-14 00:11:07 +0800

    setup: add long description

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
 from setuptools import setup, find_packages
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 
 setup(
     name='QPUIQ',
-    version='0.1',
+    version='0.1.1',
     url='https://github.com/buganini/PUI',
     author='Buganini Chiu',
     author_email='buganini@b612.tw',
     description='"PUI" Python Declarative UI Framework',
     packages=find_packages(),    
+    long_description=long_description,
+    long_description_content_type='text/markdown'
 )

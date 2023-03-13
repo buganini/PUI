e7b4c08fa138f71f114437862c6a5ae2b58f8caa
Buganini Chiu <buganini@b612.tw>
2023-03-13 23:55:05 +0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='PUI',
+    version='0.1',
+    url='https://github.com/buganini/PUI',
+    author='Buganini Chiu',
+    author_email='buganini@b612.tw',
+    description='"PUI" Python Declarative UI Framework',
+    packages=find_packages(),    
+)

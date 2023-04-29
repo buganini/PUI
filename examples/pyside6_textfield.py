14465e004ab81fc8cb9ee9669f5056e5f69918f2
Buganini Chiu <buganini@b612.tw>
2023-04-30 01:10:37 +0800

    update example for pyside6, qt5

--- examples/pyside6_textfield.py
+++ examples/pyside6_textfield.py
@@ -5,19 +5,20 @@ from PUI.PySide6 import *
 
 
 data = State()
-class QtExample(QtWindow):
+class QtExample(QtApplication):
     def __init__(self):
         super().__init__(title="blah")
         data.var = 0
 
     def content(self):
-        with QtVBox() as scope:
-            with QtHBox() as scope:
-                QtButton("-", self.on_minus)
-                QtLabel(f"{data.var}")
-                QtButton("+", self.on_plus)
+        with QtWindow():
+            with QtVBox():
+                with QtHBox():
+                    QtButton("-", self.on_minus)
+                    QtLabel(f"{data.var}")
+                    QtButton("+", self.on_plus)
 
-            QtLineEdit(data("var")) # binding
+                QtLineEdit(data("var")) # binding
 
     def on_minus(self):
         data.var -= 1
--- examples/qt5_mininal.py
+++ examples/qt5_mininal.py
@@ -5,21 +5,22 @@ from PUI.Qt5 import *
 
 
 data = State()
-class QtExample(QtWindow):
+class QtExample(QtApplication):
     def __init__(self):
         super().__init__(title="blah")
         data.var = 0
 
     def content(self):
-        with QtVBox():
-            with QtHBox():
-                QtButton("-", self.on_minus)
-                QtLabel(f"{data.var}")
-                QtButton("+", self.on_plus)
+        with QtWindow():
+            with QtVBox():
+                with QtHBox():
+                    QtButton("-", self.on_minus)
+                    QtLabel(f"{data.var}")
+                    QtButton("+", self.on_plus)
 
-            with QtHBox():
-                for i in range(0, data.var):
-                    QtLabel(f"{i}")
+                with QtHBox():
+                    for i in range(0, data.var):
+                        QtLabel(f"{i}")
 
     def on_minus(self):
         data.var -= 1

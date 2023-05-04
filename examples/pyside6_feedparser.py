185fe0d998b1497e7af40dfeb04dc1db4004c1c8
Buganini Chiu <buganini@b612.tw>
2023-05-05 01:48:27 +0800

    Add example/pyside6_feedparser.py

--- examples/pyside6_feedparser.py
+++ examples/pyside6_feedparser.py
@@ -11,45 +11,48 @@ feeds = [
     "https://hnrss.org/frontpage",
 ]
 
-data = State()
-data.selected_url = ""
-data.loaded = feedparser.FeedParserDict({
-    "feed": feedparser.FeedParserDict({
-        "title": None
-    }),
-    "entries": []
-})
-data.selected = -1
 
-def url_changed(url):
-    print("url_changed", url)
-    data.loaded = feedparser.parse(url)
 
-data("selected_url", url_changed)
-
-def entry_selected(index):
-    data.selected = index
 
 class Example(Application):
+    def setup(self):
+        self.data = State()
+        self.data.selected_url = ""
+        self.data.loaded = feedparser.FeedParserDict({
+            "feed": feedparser.FeedParserDict({
+                "title": None
+            }),
+            "entries": []
+        })
+        self.data("selected_url", self.url_changed)
+        self.data.selected = -1
+
+    def url_changed(self, url):
+        print("url_changed", url)
+        self.data.loaded = feedparser.parse(url)
+
+    def entry_selected(self, index):
+        self.data.selected = index
+
     def content(self):
         with Window(title="Feed Parser", size=(800,600)):
             with VBox():
-                with QtComboBox(text_model=data("selected_url")):
+                with QtComboBox(text_model=self.data("selected_url")):
                     for url in feeds:
                         QtComboBoxItem(url)
 
-                Label(data.loaded.feed.title).qt(StyleSheet="font-weight:bold")
+                Label(self.data.loaded.feed.title).qt(StyleSheet="font-weight:bold")
 
                 with HBox():
                     with Scroll():
                         with VBox():
-                            for i,e in enumerate(data.loaded.entries):
-                                Label(e.title).click(functools.partial(entry_selected, i))
+                            for i,e in enumerate(self.data.loaded.entries):
+                                Label(e.title).click(functools.partial(self.entry_selected, i))
                             Spacer()
 
                     with Scroll():
-                        if 0 <= data.selected and data.selected < len(data.loaded.entries):
-                            Text(data.loaded.entries[data.selected].description).qt(StyleSheet="background-color:white; color:black")
+                        if 0 <= self.data.selected and self.data.selected < len(self.data.loaded.entries):
+                            Text(self.data.loaded.entries[self.data.selected].description).qt(StyleSheet="background-color:white; color:black")
 
 
 

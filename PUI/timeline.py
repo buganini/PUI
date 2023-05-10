cb5637101f8a582a5ade71ac4d378a6362a558d5
Buganini Chiu <buganini@b612.tw>
2023-05-11 02:14:34 +0800

    Fix timer-node relation issue after TimelineView v-dom sync

--- PUI/timeline.py
+++ PUI/timeline.py
@@ -2,23 +2,33 @@ from threading import Timer
 from .view import *
 
 class TimelineView(PUINode):
+    cbmap = {}
     def __init__(self, ttl_sec):
         super().__init__()
+        self.timer = None
         self.ttl_sec = ttl_sec
 
     def update(self, prev):
         if prev and hasattr(prev, "timer"):
             self.timer = prev.timer
+            TimelineView.cbmap[self.timer] = self
         else:
             self.timer = Timer(self.ttl_sec, self.timer_cb)
+            TimelineView.cbmap[self.timer] = self
             self.timer.start()
 
     def timer_cb(self):
         if not self.timer:
             return
-        self.root.redraw()
-        self.timer = Timer(self.ttl_sec, self.timer_cb)
-        self.timer.start()
+        node = TimelineView.cbmap[self.timer] # node is changed in update()
+        del TimelineView.cbmap[self.timer]
+        root = node.root
+        if not root:
+            return
+        root.redraw()
+        node.timer = Timer(self.ttl_sec, self.timer_cb)
+        TimelineView.cbmap[self.timer] = node
+        node.timer.start()
 
     @property
     def ui(self):
@@ -33,6 +43,7 @@ class TimelineView(PUINode):
         self.timer = None
         if timer:
             timer.cancel()
+        super().destroy(direct)
 
     def addChild(self, idx, child):
         self.parent.addChild(idx, child)

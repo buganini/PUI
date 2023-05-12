a3cb1abc4d204b41b6bf976cdb2a1e79afdf9980
Buganini Chiu <buganini@b612.tw>
2023-05-12 08:16:41 +0800

    clickable TkLabel

--- PUI/tkinter/label.py
+++ PUI/tkinter/label.py
@@ -11,6 +11,14 @@ class TkLabel(TkBaseWidget):
             self.ui.config(text = self.text)
         else:
             self.ui = tk.Label(self.parent.ui, text=self.text, **self.kwargs)
+            self.ui.bind("<Button-1>", self._clicked)
+
+    def _clicked(self, *_):
+        node = self
+        while node.retired_by:
+            node = node.retired_by
+        if node.onClicked:
+            node.onClicked(*self.click_args, **self.click_kwargs)
 
     def click(self, callback, *cb_args, **cb_kwargs):
         self.onClicked = callback

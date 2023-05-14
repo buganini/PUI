abca1182902e839c5b51ac7c9a5ab161e5950167
Buganini Chiu <buganini@b612.tw>
2023-05-14 21:41:54 +0800

    update cookbook/hbox example

--- cookbook/hbox.py
+++ cookbook/hbox.py
@@ -2,7 +2,12 @@ from .config import *
 
 @PUI
 def HBoxExample():
-    with HBox():
-        Label("Column 1")
-        Spacer()
-        Label("Column 2")
+    with VBox():
+        with HBox():
+            Label("Column 1")
+            Spacer()
+            Label("Column 2")
+        with HBox():
+            Label("Column 1")
+            Label("Column 2").layout(weight=1)
+            Label("Column 3")

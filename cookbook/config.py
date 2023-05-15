9f01b28b50c5726babd59f79e1542b82123d19dc
Buganini Chiu <buganini@b612.tw>
2023-05-16 04:43:29 +0800

    cookbook: allow specifying backend

--- cookbook/config.py
+++ cookbook/config.py
@@ -1,3 +1,8 @@
-from PUI.PySide6 import *
-# from PUI.textual import *
-# from PUI.tkinter import *
+import sys
+
+if len(sys.argv)<2 or sys.argv[1] == "PySide6":
+    from PUI.PySide6 import *
+elif sys.argv[1] == "textual":
+    from PUI.textual import *
+elif sys.argv[1] == "tk":
+    from PUI.tkinter import *

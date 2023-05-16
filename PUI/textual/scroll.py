47ad8f27c9a2fa92285bd79d8b30e5d38e04c0db
Buganini Chiu <buganini@b612.tw>
2023-05-16 16:12:55 +0800

    textual: handle TScroll(vertical=,horizontal=)

--- PUI/textual/scroll.py
+++ PUI/textual/scroll.py
@@ -2,11 +2,28 @@ from .. import *
 from .base import *
 
 class TScroll(TBase):
+    def __init__(self, vertical=None, horizontal=False):
+        self.vertical = vertical
+        self.horizontal = horizontal
+        super().__init__()
+
     def update(self, prev):
         if prev and hasattr(prev, "ui"):
             self.ui = prev.ui
         else:
             self.ui = containers.ScrollableContainer()
+        v = "auto"
+        if self.vertical is True:
+            v = "scroll"
+        elif self.vertical is False:
+            v = "hidden"
+        h = "auto"
+        if self.horizontal is True:
+            h = "scroll"
+        elif self.horizontal is False:
+            h = "hidden"
+        print(f"overflow-x: {h}; overflow-y: {v};")
+        self.ui.set_styles(f"overflow-x: {h}; overflow-y: {v};")
 
     def addChild(self, idx, child):
         self.inner.mount(child.outer)

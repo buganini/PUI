343396c5a98423e12d15b77eb394a6cb99d8cf54
Buganini Chiu <buganini@b612.tw>
2023-05-18 15:14:38 +0800

    PySide6.QtLineEdit: don't setText if editing

--- PUI/PySide6/textfield.py
+++ PUI/PySide6/textfield.py
@@ -5,22 +5,39 @@ class QtLineEdit(QtBaseWidget):
     def __init__(self, model):
         super().__init__()
         self.model = model
+        self.editing = False
 
     def update(self, prev):
         value = self.model.value
         if prev and hasattr(prev, "ui"):
+            self.editing = prev.editing
             self.ui = prev.ui
+            self.ui.focusOutEvent = self.focusOutEvent
             if prev.last_value != value:
                 self.ui.textChanged.disconnect()
-                self.ui.setText(str(value))
+                if not self.editing:
+                    self.ui.setText(str(value))
                 self.ui.textChanged.connect(self.on_textchanged)
             self.last_value = value
         else:
             self.last_value = value
             self.ui = QtWidgets.QLineEdit()
+            self.ui.origFocusOutEvent = self.ui.focusOutEvent
+            self.ui.focusOutEvent = self.focusOutEvent
             self.ui.setText(str(value))
             self.ui.textChanged.connect(self.on_textchanged)
         super().update(prev)
 
     def on_textchanged(self):
-        self.model.value = self.ui.text()
+        node = self
+        while node.retired_by:
+            node = node.retired_by
+        node.editing = True
+        node.model.value = self.ui.text()
+
+    def focusOutEvent(self, event):
+        node = self
+        while node.retired_by:
+            node = node.retired_by
+        node.editing = False
+        node.ui.origFocusOutEvent(event)

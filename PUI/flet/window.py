c3298978adf68d13c6f36166479754ae0ab02180
Buganini Chiu <buganini@b612.tw>
2023-05-19 02:25:39 +0800

    flet: fix interface for FWindow

--- PUI/flet/window.py
+++ PUI/flet/window.py
@@ -2,10 +2,15 @@ from .. import *
 from .base import *
 
 class FWindow(PUINode):
-    def __init__(self, title=None, size=None):
+    def __init__(self, title=None, size=None, maximize=None, fullscreen=None):
         super().__init__()
         self.title = title
         self.size = size
+        self.curr_size = None
+        self.maximize = maximize
+        self.curr_maximize = None
+        self.fullscreen = fullscreen
+        self.curr_fullscreen = None
 
     def update(self, prev=None):
         self.parent.ui.title = self.title

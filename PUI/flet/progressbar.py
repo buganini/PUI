be01a35d4a96d12091fe40c70d3ec7258f0dd0c7
Buganini Chiu <buganini@b612.tw>
2023-05-20 01:55:13 +0800

    flet: handle FProgressBar(maximum=)

--- PUI/flet/progressbar.py
+++ PUI/flet/progressbar.py
@@ -2,14 +2,15 @@ from .. import *
 from .base import *
 
 class FProgressBar(FBase):
-    def __init__(self, progress):
+    def __init__(self, progress, maximum=1):
         super().__init__()
-        self.progress = progress
+        self.progress = progress*100
+        self.maximum = maximum*100
 
     def update(self, prev):
         if prev and hasattr(prev, "ui"):
             self.ui = prev.ui
-            self.ui.value = self.progress
+            self.ui.value = self.progress / self.maximum
             self.ui.update()
         else:
             self.ui = ft.ProgressBar()

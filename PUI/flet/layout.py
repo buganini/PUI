9662f039b238197bebb2a0a552e849d19f35b665
Buganini Chiu <buganini@b612.tw>
2023-05-22 19:17:38 +0800

    Add FColumn.flet(alignment=,horizontal_alignment), FRow.flet(alignment=,vertical_alignment=)

--- PUI/flet/layout.py
+++ PUI/flet/layout.py
@@ -20,6 +20,13 @@ class FRow(FBase):
         self.ui.controls.remove(find_flet_outer(child))
         self.ui.update()
 
+    def flet(self, alignment=None, vertical_alignment=None):
+        if not alignment is None:
+            self.flet_params["alignment"] = alignment
+        if not vertical_alignment is None:
+            self.flet_params["vertical_alignment"] = vertical_alignment
+        return self
+
 class FColumn(FBase):
     def update(self, prev):
         if prev and hasattr(prev, "ui"):
@@ -38,6 +45,16 @@ class FColumn(FBase):
     def removeChild(self, idx, child):
         self.ui.controls.remove(find_flet_outer(child))
         self.ui.update()
+
+    def flet(self, alignment=None, horizontal_alignment=None, **kwargs):
+        if not alignment is None:
+            self.flet_params["alignment"] = alignment
+        if not horizontal_alignment is None:
+            self.flet_params["horizontal_alignment"] = horizontal_alignment
+        for k,v in kwargs.items():
+            self.flet_params[k] = v
+        return self
+
 
 class FSpacer(FBase):
     def update(self, prev):

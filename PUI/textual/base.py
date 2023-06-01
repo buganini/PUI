4bbec858b55909a718aa5b224e7c5533f748bf95
Buganini Chiu <buganini@b612.tw>
2023-06-02 03:38:23 +0800

    textual: update layout sizing

--- PUI/textual/base.py
+++ PUI/textual/base.py
@@ -38,43 +38,65 @@ class TBase(PUINode):
         return parent
 
     @property
-    def fit_content_width(self):
+    def local_fit_content_width(self):
         parent = self.tparent
         if parent:
             if parent.container_x and self.layout_weight:
-                if self._debug:
-                    print("fit_content_width", False)
                 return False
-        if self._debug:
-            print("fit_content_width", True)
         return True
 
     @property
-    def fit_content_height(self):
+    def local_fit_content_height(self):
         parent = self.tparent
         if parent:
             if parent.container_y and self.layout_weight:
                 return False
         return True
 
+    @property
+    def fit_content_width(self):
+        return self.local_fit_content_width
+
+    @property
+    def fit_content_height(self):
+        return self.local_fit_content_height
+
+    @property
+    def fill_parent_width(self):
+        node = self
+        while node:
+            if isinstance(node, TBase):
+                if not node.local_fit_content_width:
+                    return True
+            if node.parent == node:
+                break
+            node = node.parent
+        return False
+
+    @property
+    def fill_parent_height(self):
+        node = self
+        while node:
+            if isinstance(node, TBase):
+                if not node.local_fit_content_height:
+                    return True
+            if node.parent == node:
+                break
+            node = node.parent
+        return False
+
     def t_update_layout(self):
         if not self.ui:
             return
 
-        fit_content_width = self.fit_content_width
-        if fit_content_width is None:
-            width = ""
-        else:
-            width = "1fr"
-            if fit_content_width:
-                width = "auto"
-            width = f"width:{width};"
-        fit_content_height = self.fit_content_height
-        if fit_content_height is None:
-            height = ""
-        else:
-            height = "1fr"
-            if fit_content_height:
-                height = "auto"
-            height = f"height:{height};"
+        fit_content_width = self.fit_content_width and not self.fill_parent_width
+        width = "1fr"
+        if fit_content_width:
+            width = "auto"
+        width = f"width:{width};"
+        fit_content_height = self.fit_content_height and not self.fill_parent_height
+        height = "1fr"
+        if fit_content_height:
+            height = "auto"
+        height = f"height:{height};"
         self.ui.set_styles(width+height)

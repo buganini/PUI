4893c6a86a195ff67a9d55115753f974da5268df
Buganini Chiu <buganini@b612.tw>
2023-06-07 01:01:28 +0800

    tkinter.TkScroll: implement scroller visibility determination

--- PUI/tkinter/scroll.py
+++ PUI/tkinter/scroll.py
@@ -3,10 +3,18 @@ from .base import *
 from tkinter.scrolledtext import ScrolledText
 
 class ScrollableFrame(tk.Frame):
-    def __init__(self, parent, vertical=True, horizontal=False):
+    def __init__(self, parent):
+        self.vertical = None
+        self.horizontal = False
+
         self.container = tk.Frame(parent)
         self.container.rowconfigure(0, weight=1)
         self.container.columnconfigure(0, weight=1)
+
+        self.scroll_y = tk.Scrollbar(self.container, orient=tk.VERTICAL)
+        self.scroll_x = tk.Scrollbar(self.container, orient=tk.HORIZONTAL)
+        self.should_scroll_y = False
+        self.should_scroll_x = False
 
         self.canvas = tk.Canvas(self.container)
         self.canvas.grid(row=0, column=0, sticky="nsew")
@@ -15,8 +23,32 @@ class ScrollableFrame(tk.Frame):
 
         self.bind("<Configure>", self._on_configure)
 
-        if vertical:
-            self.scroll_y = tk.Scrollbar(self.container, orient=tk.VERTICAL)
+
+    def set_scroll(self, vertical, horizontal):
+        self.vertical = vertical
+        self.horizontal = horizontal
+
+        self.update_scroller()
+
+    def grid(self, *args, **kwargs):
+        self.container.grid(*args, **kwargs)
+
+    def _on_configure(self, event):
+        self.update_scroller()
+
+    def update_scroller(self):
+        # self.winfo_toplevel().update_idletasks()
+
+        bbox = self.canvas.bbox("all")
+        _, _, w, h = bbox
+        self.canvas.configure(scrollregion=bbox)
+        if self.horizontal is False:
+            self.canvas.configure(width=w)
+        if self.vertical is False:
+            self.canvas.configure(height=h)
+
+        self.should_scroll_y = self.vertical or (self.vertical is None and h > self.canvas.winfo_height())
+        if self.should_scroll_y:
             self.scroll_y.config(command=self.canvas.yview)
             self.canvas.config(yscrollcommand=self.scroll_y.set)
             self.scroll_y.grid(row=0, column=1, sticky="ns")
@@ -24,25 +56,20 @@ class ScrollableFrame(tk.Frame):
             self.canvas.bind_all("<MouseWheel>", self._on_mousewheel_y)
             self.canvas.bind("<Button-4>", self._on_mousewheel_y)
             self.canvas.bind("<Button-5>", self._on_mousewheel_y)
+        else:
+            self.scroll_y.grid_forget()
 
-        if horizontal:
-            self.scroll_x = tk.Scrollbar(self.container, orient=tk.HORIZONTAL)
+        self.should_scroll_x = self.horizontal or (self.horizontal is None and w > self.canvas.winfo_width())
+        if self.should_scroll_x:
             self.scroll_x.config(command=self.canvas.xview)
             self.canvas.config(xscrollcommand=self.scroll_x.set)
             self.scroll_x.grid(row=1, column=0, sticky="ew")
-
-    def grid(self, *args, **kwargs):
-        self.container.grid(*args, **kwargs)
-
-    def _on_configure(self, event):
-        self.update_layout()
-
-    def update_layout(self):
-        bbox = self.canvas.bbox("all")
-        self.canvas.configure(scrollregion=bbox)
-        self.canvas.configure(width=bbox[2])
+        else:
+            self.scroll_x.grid_forget()
 
     def _on_mousewheel_y(self, event):
+        if not self.should_scroll_y:
+            return
         if event.num == 5:
             delta = 1
         elif event.num == 4:
@@ -70,20 +97,6 @@ class TkScroll(TkBaseWidget):
         if idx:
             return
         child.outer.grid(row=idx, column=0, sticky='nsew')
-
-    def postUpdate(self):
-        # if not self.layout_weight:
-        #     parent = self
-        #     while True:
-        #         nparent = parent.tkparent
-        #         if nparent:
-        #             parent = nparent
-        #         else:
-        #             break
-
-        #     parent.ui.update_idletasks()
-            # self.canvas.config(width=self.frame.winfo_reqwidth(), height=self.frame.winfo_reqheight())
-        super().postUpdate()
 
     def removeChild(self, idx, child):
         child.outer.grid_forget()

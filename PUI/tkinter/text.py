b9a4cb43ecfe3b1ed22bf3998b1da364364206d7
Buganini Chiu <buganini@b612.tw>
2023-06-07 00:37:24 +0800

    add missing file for tkinter.TkText

---
+++ PUI/tkinter/text.py
@@ -0,0 +1,14 @@
+from .. import *
+from .base import *
+
+class TkText(TkBaseWidget):
+    def __init__(self, text):
+        super().__init__()
+        self.text = text
+
+    def update(self, prev):
+        if prev and prev.ui:
+            self.ui = prev.ui
+            self.ui.config(text = self.text)
+        else:
+            self.ui = tk.Label(self.parent.ui, text=self.text)

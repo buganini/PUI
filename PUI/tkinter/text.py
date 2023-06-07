70b3f5c11ee451dabdeadae4f80f7a2c57a50182
Buganini Chiu <buganini@b612.tw>
2023-06-07 17:20:02 +0800

    tkinter.TkText/TkLabel: left align text

--- PUI/tkinter/text.py
+++ PUI/tkinter/text.py
@@ -11,4 +11,4 @@ class TkText(TkBaseWidget):
             self.ui = prev.ui
             self.ui.config(text = self.text)
         else:
-            self.ui = tk.Label(self.parent.ui, text=self.text)
+            self.ui = tk.Label(self.parent.ui, text=self.text, anchor="w", justify="left")

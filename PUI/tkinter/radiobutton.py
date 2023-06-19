1ca79a0d894f6c42064efd51299429aa5a60ce2b
Buganini Chiu <buganini@b612.tw>
2023-06-19 12:00:04 +0800

    fix tkinter.RadioButton

--- PUI/tkinter/radiobutton.py
+++ PUI/tkinter/radiobutton.py
@@ -4,19 +4,20 @@ from .base import *
 class RadioButton(TkBaseWidget):
     def __init__(self, text, value, model):
         super().__init__()
+        self.var = None
         self.text = text
         self.value = value
         self.model = model
 
     def update(self, prev):
         if prev and prev.ui:
+            self.var = prev.var
             self.ui = prev.ui
         else:
-            self.ui = tk.Radiobutton(self.tkparent.inner, text=self.text, anchor="w")
-        if self.value  == self.model.value:
-            self.ui.select()
-        else:
-            self.ui.deselect()
+            self.var = tk.StringVar()
+            self.ui = tk.Radiobutton(self.tkparent.inner, text=self.text, variable=self.var, value=self.value, anchor="w")
+
+        self.var.set(self.model.value)
         self.ui.configure(command=self._select)
         super().update(prev)
 

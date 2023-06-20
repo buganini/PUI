87f6bcbc1475dc6db213a503c9709f9b0f650409
Buganini Chiu <buganini@b612.tw>
2023-06-20 19:45:41 +0800

    minor text update in cookbook

--- cookbook/text.py
+++ cookbook/text.py
@@ -5,4 +5,4 @@ def TextExample():
     with VBox():
         Text("Multiple\nLines\nText")
         Html('<i>HTML</i> <a style="color:red">support</a>')
-        MarkDown("# H1\n## H2\n* item1\n* item 2")
+        MarkDown("# H1\n## H2\n* item 1\n* item 2")

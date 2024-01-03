5e2e0fefae6674296ddafdcd7edfa6e9dd86d00c
Buganini Chiu <buganini@b612.tw>
2024-01-03 21:15:04 +0800

    PySide6.table: move header show/hide control out of table model

--- PUI/PySide6/table.py
+++ PUI/PySide6/table.py
@@ -2,54 +2,54 @@ from .. import *
 from .base import *
 
 class QtTableModelAdapter(QtCore.QAbstractTableModel):
-    def __init__(self, node):
+    def __init__(self, model):
         super().__init__()
-        self.node = node
+        self.model = model
 
     def data(self, index, role):
         if role == QtCore.Qt.DisplayRole:
-            return self.node.model.data(index.row(), index.column())
+            return self.model.data(index.row(), index.column())
         elif role == QtCore.Qt.EditRole:
-            if hasattr(self.node.model, "editData"):
-                return self.node.model.editData(index.row(), index.column())
+            if hasattr(self.model, "editData"):
+                return self.model.editData(index.row(), index.column())
             else:
-                return self.node.model.data(index.row(), index.column())
+                return self.model.data(index.row(), index.column())
 
     def setData(self, index, value, role):
         if role == QtCore.Qt.EditRole:
-            self.node.model.setData(index.row(), index.column(), value)
+            self.model.setData(index.row(), index.column(), value)
             return True
 
     def flags(self, index):
         flags = super().flags(index)
-        if self.node.model.editable(index.row(), index.column()):
+        if self.model.editable(index.row(), index.column()):
             flags |= QtCore.Qt.ItemIsEditable
         return flags
 
     def headerData(self, section, orientation, role):
         if role == QtCore.Qt.DisplayRole:
             if orientation == QtCore.Qt.Horizontal:
-                if hasattr(self.node.model, "columnHeader"):
-                    if self.node.model.columnHeader is None:
-                        return self.node.ui.horizontalHeader().hide()
+                if hasattr(self.model, "columnHeader"):
+                    if self.model.columnHeader is None:
+                        return None
                     else:
-                        return self.node.model.columnHeader(section)
+                        return self.model.columnHeader(section)
                 else:
                     return super().headerData(section, orientation, role)
             else:
-                if hasattr(self.node.model, "rowHeader"):
-                    if self.node.model.rowHeader is None:
-                        return self.node.ui.verticalHeader().hide()
+                if hasattr(self.model, "rowHeader"):
+                    if self.model.rowHeader is None:
+                        return None
                     else:
-                        return self.node.model.rowHeader(section)
+                        return self.model.rowHeader(section)
                 else:
                     return super().headerData(section, orientation, role)
 
     def rowCount(self, index):
-        return self.node.model.rowCount()
+        return self.model.rowCount()
 
     def columnCount(self, index):
-        return self.node.model.columnCount()
+        return self.model.columnCount()
 
 
 class Table(QtBaseWidget):
@@ -61,10 +61,19 @@ class Table(QtBaseWidget):
     def update(self, prev):
         if prev and prev.ui:
             self.ui = prev.ui
-            self.qt_model.node = self
         else:
             self.ui = QtWidgets.QTableView()
-            self.qt_model = QtTableModelAdapter(self)
+            self.qt_model = QtTableModelAdapter(self.model)
+
+        if self.model.columnHeader is None:
+            self.ui.horizontalHeader().hide()
+        else:
+            self.ui.horizontalHeader().show()
+
+        if self.model.rowHeader is None:
+            self.ui.verticalHeader().hide()
+        else:
+            self.ui.verticalHeader().show()
 
         self.ui.setModel(self.qt_model)
         if self.autofit:

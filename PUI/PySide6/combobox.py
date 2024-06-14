e2b4c537411137e6b596e036f7d5a8f4b0ebfc02
Buganini Chiu <buganini@b612.tw>
2024-06-14 15:21:28 +0800

    fix initial state for PySide6.Combobox

--- PUI/PySide6/combobox.py
+++ PUI/PySide6/combobox.py
@@ -6,10 +6,6 @@ class ComboBox(QtBaseWidget):
     def __init__(self, editable=False, index_model=None, text_model=None):
         super().__init__()
         self.editable = editable
-        if index_model is None:
-            index_model = DummyBinding(0)
-        if text_model is None:
-            text_model = DummyBinding("")
         self.index_model = index_model
         self.text_model = text_model
         self.index_changed = None
@@ -23,37 +19,55 @@ class ComboBox(QtBaseWidget):
         return self
 
     def update(self, prev):
-        index = self.index_model.value
-        text = self.text_model.value
         if prev and prev.ui:
+            self.last_index = prev.last_index
+            self.last_text = prev.last_text
+            self.signal_connected = prev.signal_connected
             self.ui = prev.ui
-            if prev.last_index != index:
-                self.ui.currentIndexChanged.disconnect()
-                self.ui.setCurrentIndex(index)
-                self.ui.currentIndexChanged.connect(self.on_currentIndexChanged)
-            self.last_index = index
-
-            if prev.last_text != text:
-                self.ui.currentTextChanged.disconnect()
-                self.ui.setCurrentText(text)
-                self.ui.currentTextChanged.connect(self.on_currentTextChanged)
-            self.last_text = text
         else:
-            self.last_index = index
-            self.last_text = text
+            self.last_index = None
+            self.last_text = None
+            self.signal_connected = False
             self.ui = QtWidgets.QComboBox()
-            self.ui.currentIndexChanged.connect(self.on_currentIndexChanged)
-            self.ui.currentTextChanged.connect(self.on_currentTextChanged)
         self.ui.setEditable(self.editable)
         super().update(prev)
 
+    def postSync(self):
+        if self.index_model:
+            index = self.index_model.value
+            text = self.children[index].text
+        elif self.text_model:
+            text = self.text_model.value
+            try:
+                index = [c.text for c in self.children].index(text)
+            except:
+                index = 0
+
+        if self.signal_connected:
+            self.ui.currentIndexChanged.disconnect()
+            self.ui.currentTextChanged.disconnect()
+
+        if self.last_index != index:
+            self.ui.setCurrentIndex(index)
+        self.last_index = index
+
+        if self.last_text != text:
+            self.ui.setCurrentText(text)
+        self.last_text = text
+
+        self.ui.currentIndexChanged.connect(self.on_currentIndexChanged)
+        self.ui.currentTextChanged.connect(self.on_currentTextChanged)
+
+
     def on_currentIndexChanged(self, idx):
-        self.index_model.value = idx
+        if self.index_model:
+            self.index_model.value = idx
         if self.index_changed:
             self.index_changed(idx)
 
     def on_currentTextChanged(self, text):
-        self.text_model.value = text
+        if self.text_model:
+            self.text_model.value = text
         if self.text_changed:
             self.text_changed(text)
 

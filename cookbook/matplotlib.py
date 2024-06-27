3a7c90cb435529b50fa77986422dc922916ddaa4
Buganini Chiu <buganini@b612.tw>
2024-06-27 14:07:37 +0800

    update MatplotlibCanvas example

--- cookbook/matplotlib.py
+++ cookbook/matplotlib.py
@@ -1,12 +1,12 @@
 from .config import *
-
 class MatplotlibCanvasExample(PUIView):
     def content(self):
         data = [(0,0), (1,3), (2,2)]
         MatplotlibCanvas(self.plot, data).layout(weight=1)
 
-    @staticmethod
-    def plot(figure, data):
+    @classmethod
+    def plot(cls, figure, data):
         figure.clear()
         sp = figure.add_subplot(111)
-        sp.axes.plot([d[0] for d in data], [d[1] for d in data])
+        lines = sp.axes.plot([d[0] for d in data], [d[1] for d in data])
+        sp.axes.grid()

3e8d3e78bb0b85ed0db0a766a33952e8a966848b
Buganini Chiu <buganini@b612.tw>
2024-07-05 02:21:53 +0800

    PySide6: implement Image sizing

--- PUI/PySide6/image.py
+++ PUI/PySide6/image.py
@@ -18,6 +18,13 @@ class Image(QtBaseWidget):
 
         if self.curr_path.set(self.path):
             self.pixmap = QtGui.QPixmap(self.path)
+            if self.layout_width is not None and self.layout_height is not None:
+                self.pixmap = self.pixmap.scaled(self.layout_width, self.layout_height, QtCore.Qt.KeepAspectRatio)
+            elif self.layout_width is not None:
+                self.pixmap = self.pixmap.scaledToWidth(self.layout_width)
+            elif self.layout_height is not None:
+                self.pixmap = self.pixmap.scaledToHeight(self.layout_height)
+
             self.ui.setPixmap(self.pixmap)
 
         super().update(prev)
--- cookbook/image.py
+++ cookbook/image.py
@@ -3,5 +3,5 @@ from .config import *
 class ImageExample(PUIView):
     def content(self):
         with VBox():
-            Image("screenshots/hello_world.png")
+            Image("screenshots/hello_world.png").layout(height=360)
             Spacer()

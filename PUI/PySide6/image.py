e234bddf4872b4f3e08735d5c770f68de081c8f5
Buganini Chiu <buganini@b612.tw>
2024-07-05 02:23:46 +0800

    PySide6.Image: use SmoothTransformation instead of default FastTransformation

--- PUI/PySide6/image.py
+++ PUI/PySide6/image.py
@@ -19,11 +19,11 @@ class Image(QtBaseWidget):
         if self.curr_path.set(self.path):
             self.pixmap = QtGui.QPixmap(self.path)
             if self.layout_width is not None and self.layout_height is not None:
-                self.pixmap = self.pixmap.scaled(self.layout_width, self.layout_height, QtCore.Qt.KeepAspectRatio)
+                self.pixmap = self.pixmap.scaled(self.layout_width, self.layout_height, QtCore.Qt.KeepAspectRatio, mode=QtCore.Qt.SmoothTransformation)
             elif self.layout_width is not None:
-                self.pixmap = self.pixmap.scaledToWidth(self.layout_width)
+                self.pixmap = self.pixmap.scaledToWidth(self.layout_width, mode=QtCore.Qt.SmoothTransformation)
             elif self.layout_height is not None:
-                self.pixmap = self.pixmap.scaledToHeight(self.layout_height)
+                self.pixmap = self.pixmap.scaledToHeight(self.layout_height, mode=QtCore.Qt.SmoothTransformation)
 
             self.ui.setPixmap(self.pixmap)
 

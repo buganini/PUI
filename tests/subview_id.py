2983df516f58747bc52cd4c1af3c5d02195e32e4
Buganini Chiu <buganini@b612.tw>
2024-08-09 17:13:38 +0800

    add subview_id test

---
+++ tests/subview_id.py
@@ -0,0 +1,61 @@
+import sys
+sys.path.append("..")
+
+from PUI.PySide6 import *
+import time
+
+n = 2
+
+data = State()
+data.destroyed = [False] * n
+data.destroyed_id = [False] * n
+
+class Subview(PUIView):
+    def __init__(self, ident):
+        super().__init__()
+        self.ident = ident
+
+    def content(self):
+        print(f"content {self.ident}")
+        print(f"Subview{self.ident}.content")
+        Label(f"Subview{self.ident}")
+
+class Example(Application):
+    def __init__(self, icon=None):
+        super().__init__(icon)
+
+    def content(self):
+        with Window(title="blah"):
+            with VBox():
+                Label("Without Id")
+
+                for i in range(n):
+                    if not data.destroyed[i]:
+                        Subview(i)
+
+                with HBox(): 
+                    for i in range(n):
+                        Button(f"Destroy {i}").click(self.do_test, i)
+
+                Label("With Id")
+                with VBox():
+                    for i in range(n):
+                        if not data.destroyed_id[i]:
+                            Subview(1000+i).id(1000+i)
+
+                with HBox(): 
+                    for i in range(n):
+                        Button(f"Destroy {i}").click(self.do_test_id, i)
+
+             
+                Label("Expect: Subview be gone")
+                Label("Misbehavior: Subview become standalone window")
+
+    def do_test(self, e, i):
+        data.destroyed[i] = not data.destroyed[i]
+
+    def do_test_id(self, e, i):
+        data.destroyed_id[i] = not data.destroyed_id[i]
+
+root = Example()
+root.run()

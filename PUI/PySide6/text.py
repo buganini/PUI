95fb5a64a8e0042a0f7bda72aefb9dcbb9eea62d
Buganini Chiu <buganini@b612.tw>
2024-08-31 23:56:20 +0800

    PySide6.text: fix sizing

--- PUI/PySide6/text.py
+++ PUI/PySide6/text.py
@@ -3,41 +3,22 @@ from .base import *
 from ..utils import *
 from PySide6.QtWidgets import QSizePolicy
 
-class QText(QtWidgets.QLabel):
-    def __init__(self, text, sizeHint):
-        self._sizehint = sizeHint
-        super().__init__(text)
-
-    def sizeHint(self):
-        return QtCore.QSize(*self._sizehint)
-
-    def minimumSizeHint(self):
-        return QtCore.QSize(*self._sizehint)
-    
-    def setSizeHint(self, sizeHint):
-        if sizeHint != self._sizehint:
-            self._sizehint = sizeHint
-            self.adjustSize()
-
 class Text(QtBaseWidget):
     textformat = QtCore.Qt.TextFormat.PlainText
-    def __init__(self, text, selectable=False, sizeHint=(120,320)):
+    def __init__(self, text, selectable=False):
         super().__init__()
         self.text = str(text)
         self.selectable = selectable
-        self.sizeHint = sizeHint
 
     def update(self, prev):
         if prev and prev.ui:
             self.ui = prev.ui
             self.ui.setText(self.text)
-            self.ui.setSizeHint(self.sizeHint)
         else:
-            self.ui = QText(self.text, self.sizeHint)
+            self.ui = QtWidgets.QLabel(self.text)
             self.ui.setTextFormat(self.textformat)
             self.ui.setAlignment(QtCore.Qt.AlignmentFlag.AlignTop)
             self.ui.setWordWrap(True)
-            self.ui.setSizePolicy(QSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Ignored))
 
         if self.selectable:
             self.ui.setTextInteractionFlags(QtCore.Qt.TextInteractionFlag.TextSelectableByMouse)
--- cookbook/text.py
+++ cookbook/text.py
@@ -6,3 +6,4 @@ def TextExample():
         Text("Multiple\nLines\nText")
         Html('<i>HTML</i> <a style="color:red">support</a>')
         MarkDown("# H1\n## H2\n* item 1\n* item 2")
+        Spacer()

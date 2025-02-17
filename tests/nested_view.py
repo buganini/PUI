40683cb5e5ab711277aade4ef4154df889eca1e8
Buganini Chiu <buganini@b612.tw>
2025-02-17 21:36:13 +0100

    update tests/nested_view.py

--- tests/nested_view.py
+++ tests/nested_view.py
@@ -6,35 +6,73 @@ import time
 
 data = State()
 
-mainview_cnt = 0
-subview_cnt = 0
+view1_cnt = 0
+view2_cnt = 0
+view3_cnt = 0
+view4_cnt = 0
 
-class Subview(PUIView):
+class View4(PUIView):
     def content(self):
-        global subview_cnt
-        subview_cnt += 1
-        Button("Redraw subview").click(self.do_redraw)
-        Label(f"subview {subview_cnt}")
+        global view4_cnt
+        print("View4.content()")
+        view4_cnt += 1
+        Label(f"View4 {view4_cnt}")
+        Button("Redraw View4").click(self.do_redraw)
 
     def do_redraw(self, e):
+        print("redraw", self.key)
         self.redraw()
 
-class Example(Application):
-    def __init__(self):
-        super().__init__()
-
-
+class View3(PUIView):
     def content(self):
-        global mainview_cnt
-        mainview_cnt += 1
+        global view3_cnt
+        print("View3.content()")
+        view3_cnt += 1
+        Label(f"View3 {view3_cnt}")
+        Button("Redraw View3").click(self.do_redraw)
+        View4()
+
+    def do_redraw(self, e):
+        print("redraw", self.key)
+        self.redraw()
+
+class View2(PUIView):
+    def content(self):
+        global view2_cnt
+        print("View2.content()")
+        view2_cnt += 1
+        Label(f"View2 {view2_cnt}")
+        Button("Redraw View2").click(self.do_redraw)
+        View3()
+
+    def do_redraw(self, e):
+        print("redraw", self.key)
+        self.redraw()
+
+class View1(Application):
+    def content(self):
+        global view1_cnt
+        print("View1.content()")
+        view1_cnt += 1
         with Window(title="blah"):
             with VBox():
-                Button("Redraw mainview").click(self.do_redraw)
-                Label(f"mainview {mainview_cnt}")
-                Subview()
+                Button("Reset").click(self.do_reset)
+                Label(f"View1 {view1_cnt}")
+                Button("Redraw View1").click(self.do_redraw)
+                View2()
 
     def do_redraw(self, e):
+        print("redraw", self.key)
         self.redraw()
 
-root = Example()
+    def do_reset(self, e):
+        global view1_cnt
+        global view2_cnt
+        global view3_cnt
+        view1_cnt = 0
+        view2_cnt = 0
+        view3_cnt = 0
+        self.redraw()
+
+root = View1()
 root.run()

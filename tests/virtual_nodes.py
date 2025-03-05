d18effe5422d88d4452e498f09858a37cf64da89
Buganini Chiu <buganini@b612.tw>
2025-03-06 02:00:20 +0800

    update tests/virtual_nodes.py

--- tests/virtual_nodes.py
+++ tests/virtual_nodes.py
@@ -1,38 +1,62 @@
 import sys
 sys.path.append("..")
 
-from datetime import datetime
 from PUI.PySide6 import *
+import time
 
-class Subview1(PUIView):
+data = State()
+data.show = True
+data.count1 = 0
+data.count2 = 0
+
+class View1(PUIView):
     def content(self):
-        Label(f"Subview1.Label")
+        for i in range(data.count1):
+            Label(f"View1-{i}")
 
-class Subview3(PUIView):
+class View2(PUIView):
     def content(self):
-        Label(f"Subview3.Label")
+        for i in range(data.count2):
+            Label(f"View2-{i}")
 
-class Subview2(PUIView):
-    def content(self):
-        Subview3()
-
-class TimelineSubview(PUIView):
-    def content(self):
-        with TimelineView(ttl_sec=5): # virtual
-            Label(f"TimlineSubView.Label {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}")
-
-class VirtualTest(Application):
+class App(Application):
     def content(self):
         with Window(title="blah"):
             with VBox():
-                Subview1() # virtual
+                with HBox():
+                    Button("-").click(self.do_decrease1)
+                    Button("+").click(self.do_increase1)
+                View1()
 
-                Subview2() # nested-virtual
+                with HBox():
+                    Button("Hide").click(self.do_hide)
+                    Button("Show").click(self.do_show)
 
-                TimelineSubview() # nested-virtual
+                if data.show:
+                    Label("*****")
 
-                with TimelineView(ttl_sec=5): # virtual
-                    Label(f"TimlineView.Label {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}")
+                with HBox():
+                    Button("-").click(self.do_decrease2)
+                    Button("+").click(self.do_increase2)
+                View2()
 
-root = VirtualTest()
+    def do_hide(self, e):
+        data.show = False
+
+    def do_show(self, e):
+        data.show = True
+
+    def do_decrease1(self, e):
+        data.count1 -= 1
+
+    def do_increase1(self, e):
+        data.count1 += 1
+
+    def do_decrease2(self, e):
+        data.count2 -= 1
+
+    def do_increase2(self, e):
+        data.count2 += 1
+
+root = App()
 root.run()

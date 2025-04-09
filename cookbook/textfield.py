fe884f009badd66cdd8ac13e3f0b417432a0367a
Buganini Chiu <buganini@b612.tw>
2025-04-09 10:48:18 +0800

    update textfield cookbook

--- cookbook/textfield.py
+++ cookbook/textfield.py
@@ -4,9 +4,20 @@ state = State()
 state.text = "test"
 state.editing = ""
 
+def clear_state(e):
+    state.text = ""
+
+def set_something(e):
+    state.text = "something"
+
 @PUI
 def TextFieldExample():
     with VBox():
+        with HBox():
+            Button("Clear").click(clear_state)
+            Button("Set something").click(set_something)
+            Spacer()
+
         Label("State Model")
         Label("State:" + state.text)
         (TextField(state("text"))

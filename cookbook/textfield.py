0f430a92280d54ff8976bf941dc7d7345fd76515
Buganini Chiu <buganini@b612.tw>
2025-04-09 10:44:56 +0800

    update textfield cookbook

--- cookbook/textfield.py
+++ cookbook/textfield.py
@@ -7,12 +7,15 @@ state.editing = ""
 @PUI
 def TextFieldExample():
     with VBox():
-        Label("State")
-        Label(state.text)
+        Label("State Model")
+        Label("State:" + state.text)
         (TextField(state("text"))
             .input(lambda e: print("input", e))
             .change(lambda e: print("change", e)))
-        Label("Editing Buffer")
-        Label(state.editing)
+
+        Label("Editing Buffer Model")
+        Label("State:" + state.text)
+        Label("Editing:" + state.editing)
         TextField(state("text"), state("editing"))
+
         Spacer()

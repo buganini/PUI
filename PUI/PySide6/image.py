79990c7ece9efbeef69244fdcbee52f3718a0262
Buganini Chiu <buganini@b612.tw>
2025-05-07 01:02:03 +0800

    PySide6.Image: refresh when file mtime is changed

--- PUI/PySide6/image.py
+++ PUI/PySide6/image.py
@@ -1,6 +1,7 @@
 from .. import *
 from .base import *
 from .label import ClickableQLabel
+import os
 
 class Image(QtBaseWidget):
     def __init__(self, path):
@@ -13,16 +14,18 @@ class Image(QtBaseWidget):
             self.ui = prev.ui
             self.eventFilter = prev.eventFilter
             self.curr_path = prev.curr_path
+            self.curr_path_mtime = prev.curr_path_mtime
             self.pixmap = prev.pixmap
         else:
             self.ui = ClickableQLabel()
             self.ui.clicked.connect(self._clicked)
             self.curr_path = Prop()
+            self.curr_path_mtime = Prop()
 
         if self._onClicked:
             self.ui.setCursor(QtCore.Qt.PointingHandCursor)
 
-        if self.curr_path.set(self.path):
+        if self.curr_path.set(self.path) or self.curr_path_mtime.set(os.path.getmtime(self.path)):
             self.pixmap = QtGui.QPixmap(self.path)
             if self.layout_width is not None and self.layout_height is not None:
                 self.pixmap = self.pixmap.scaled(self.layout_width, self.layout_height, QtCore.Qt.KeepAspectRatio, mode=QtCore.Qt.SmoothTransformation)
